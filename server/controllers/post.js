d0303ef3d5bea3f8098b26977a847ec8fbb0963d
Leo <aammlj2002@gmail.com>
2022-04-06 11:10:47 +0630

    add like post api in backend

--- server/controllers/post.js
+++ server/controllers/post.js
@@ -21,14 +21,14 @@ const create = async (req, res) => {
 const update = async (req, res) => {
     const { id } = req.params;
     const post = req.body;
-    if (!mongoose.Types.ObjectId.isValid(id)) {
+    if (!mongoose.isValidObjectId(id)) {
         return res.status(404).send("post with this id not found");
     }
     try {
         const updatedPost = await Post.findByIdAndUpdate(id, post, {
             new: true,
         });
-        res.json(updatedPost);
+        return res.send(200).json(updatedPost);
     } catch (error) {
         res.status(409).json({ message: error.message });
     }
@@ -50,4 +50,24 @@ const destroy = async (req, res) => {
         res.status(409).json({ message: error.message });
     }
 };
-export { index, create, update, destroy };
+const addLike = async (req, res) => {
+    const { id } = req.params;
+    if (!mongoose.isValidObjectId(id)) {
+        return res.status(404).send("post with this id not found");
+    }
+    try {
+        const post = await Post.findById(id);
+        console.log(post);
+        const likedPost = await Post.findByIdAndUpdate(
+            id,
+            {
+                likeCount: post.likeCount + 1,
+            },
+            { new: true }
+        );
+        return res.status(200).json(likedPost);
+    } catch (error) {
+        res.status(409).json({ message: error.message });
+    }
+};
+export { index, create, update, destroy, addLike };
--- server/routes/posts.js
+++ server/routes/posts.js
@@ -1,10 +1,17 @@
 import express from "express";
-import { index, create, update, destroy } from "../controllers/post.js";
+import {
+    index,
+    create,
+    update,
+    destroy,
+    addLike,
+} from "../controllers/post.js";
 const router = express.Router();
 
 router.get("/", index);
 router.post("/create", create);
 router.patch("/update/:id", update);
 router.delete("/delete/:id", destroy);
+router.patch("/like/:id", addLike);
 
 export default router;
