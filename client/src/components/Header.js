c958a512bcc313b3eb26c68c4fb41d58c3ebc141
Leo <aammlj2002@gmail.com>
2022-04-07 15:21:44 +0630

    show user information in header

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,6 +1,7 @@
 import React from "react";
 
 function Header() {
+    const profile = JSON.parse(localStorage.getItem("profile"));
     return (
         <>
             <div>
@@ -34,20 +35,25 @@ function Header() {
                                     </div>
                                 </div>
                             </div>
-                            {/* search box */}
-                            <div className="relative flex items-center w-full h-full lg:w-64 group">
-                                <svg
-                                    className="absolute left-0 z-20 block w-4 h-4 ml-4 text-gray-400 pointer-events-none fill-current "
-                                    xmlns="http://www.w3.org/2000/svg"
-                                    viewBox="0 0 20 20"
-                                >
-                                    <path d="M12.9 14.32a8 8 0 1 1 1.41-1.41l5.35 5.33-1.42 1.42-5.33-5.34zM8 14A6 6 0 1 0 8 2a6 6 0 0 0 0 12z"></path>
-                                </svg>
-                                <input
-                                    type="text"
-                                    className="block  w-full py-1.5 pl-10 pr-4 leading-normal rounded-2xl focus:border-transparent focus:outline-none focus:ring-2 focus:ring-gray-500 ring-opacity-90  bg-gray-300 text-gray-400 aa-input"
-                                    placeholder="Search"
-                                />
+                            <div className="flex">
+                                {/* search box */}
+                                <div className="relative flex items-center w-full h-full lg:w-64 group">
+                                    <svg
+                                        className="absolute left-0 z-20 block w-4 h-4 ml-4 text-gray-400 pointer-events-none fill-current "
+                                        xmlns="http://www.w3.org/2000/svg"
+                                        viewBox="0 0 20 20"
+                                    >
+                                        <path d="M12.9 14.32a8 8 0 1 1 1.41-1.41l5.35 5.33-1.42 1.42-5.33-5.34zM8 14A6 6 0 1 0 8 2a6 6 0 0 0 0 12z"></path>
+                                    </svg>
+                                    <input
+                                        type="text"
+                                        className="block  w-full py-1.5 pl-10 pr-4 leading-normal rounded-2xl focus:border-transparent focus:outline-none focus:ring-2 focus:ring-gray-500 ring-opacity-90  bg-gray-300 text-gray-400 aa-input"
+                                        placeholder="Search"
+                                    />
+                                </div>
+                                <div className="flex items-center ml-5">
+                                    {profile ? profile.result.name : ""}
+                                </div>
                             </div>
                         </div>
                     </div>
